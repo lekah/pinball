ab8c56380602851f51a578873ab259585d13ccee
Leonid Kahle <leonid.kahle@epfl.ch>
2016-10-17 18:22:39 +0200

    Cleaning up the unused variable in run_pwscf.f90

--- espresso-5.2.0/PW/src/run_pwscf.f90
+++ espresso-5.2.0/PW/src/run_pwscf.f90
@@ -36,75 +36,11 @@ SUBROUTINE run_pwscf ( exit_status )
   USE mp_images,        ONLY : intra_image_comm
   USE qmmm,             ONLY : qmmm_initialization, qmmm_shutdown, &
                                qmmm_update_positions, qmmm_update_forces
-  USE lsda_mod,         ONLY : nspin                ! LEONID
-  USE io_rho_xml,       ONLY : read_rho             ! LEONID
-  USE scf,              ONLY : rho, charge_density, vltot    ! LEONID
-  USE kinds,            ONLY : DP                   ! LEONID
-  USE fft_base,         ONLY : dfftp                ! LEONID
-  USE mp,               ONLY : mp_sum               ! LEONID
-  USE mp_bands,         ONLY : intra_bgrp_comm      ! LEONID
-  USE cell_base,        ONLY : omega                ! LEONID
-  ! USE io_files,         ONLY : prefix               ! LEONID
-  !~   USE input_parameters, ONLY : prefix_flipper_charge          ! LEONID
-  ! USE vlocal,           ONLY : strf, vloc                               ! LEONID
-  ! USE ions_base,        ONLY : nat, ityp, tau, ntyp => nsp, zv          ! LEONID
-  USE gvect,            ONLY :  ngm, gstart, g, eigts1, &
-                                eigts2, eigts3, gcutm, &
-                                gg,ngl, nl, igtongl                     ! LEONID
-  USE cell_base,        ONLY : bg, at, alat                             ! LEONID
-  USE fft_interfaces,   ONLY : fwfft
-  
-  USE force_mod,      ONLY : force 
-  USE wavefunctions_module,  ONLY: psic, evc !Aris
-  use wvfct, only : nbnd, wg
-  USE pinball
+  USE pinball,          ONLY : deallocate_flipper, lflipper, flipper_forces_potener
   USE hustler,          ONLY : init_hustler, end_hustler, lhustle
-  
- ! For temp check
-   USE fft_base,        ONLY : dffts
-   USE gvecs,           ONLY : nls, nlsm
-   USE wvfct,           ONLY : npw, igk
-   USE fft_interfaces,  ONLY : invfft
-   USE cell_base,       ONLY : omega
-   use io_files,        ONLY : nwordwfc,diropn,iunwfc
-   USE klist,           only : xk
-   USE uspp,     ONLY : vkb, nkb
-   
-   ! END for temp check
 
   IMPLICIT NONE
   INTEGER, INTENT(OUT) :: exit_status
-  !
-  ! for temp check
-  
-  LOGICAL :: exst
-  INTEGER, EXTERNAL :: find_free_unit
-  INTEGER :: iv, i, iun
-  REAL(DP) :: q_tot, q_tot2, q_tot3
-  REAL(DP), allocatable :: charge2(:)
-  ! end temp check
-  
-  !
-  ! START LEONID
-!  LOGICAL  :: lflipper                                  ! LEONID
-  INTEGER  :: igrid,iatom,igm                                     ! LEONID
-  INTEGER  :: ntyp_save                                 ! LEONID
-!  INTEGER  :: nr_of_pinballs                                  ! LEONID
-  INTEGER  :: counter                                   ! LEONID
-  INTEGER  :: na                                   ! LEONID
-  INTEGER  :: ipol                                   ! LEONID
-!  REAL(DP) :: charge_density_diff, charge_all           ! LEONID
-!  REAL(DP) :: flipper_energy_external, flipper_ewld_energy     ! LEONID
-!  REAL(DP) :: flipper_ewld_forces                       ! LEONID
-  CHARACTER(LEN=256) :: normal_prefix                   ! LEONID
-  REAL(DP), EXTERNAL :: ewald                           ! LEONID
-!  REAL(DP), ALLOCATABLE :: flipper_ewald_force(:,:)                ! LEONID
-!  REAL(DP), ALLOCATABLE :: flipper_forcelc(:,:)                ! LEONID
-!  REAL(DP), ALLOCATABLE :: total_force(:,:)                ! LEONID
-!  REAL(DP), ALLOCATABLE :: aux_rho_of_r(:,:)                ! LEONID
-!  COMPLEX(DP), ALLOCATABLE :: charge_g(:)                ! LEONID
-  
-  
 
 
   
@@ -207,62 +143,9 @@ SUBROUTINE run_pwscf ( exit_status )
      ! LEONID
      IF (lflipper) THEN
 
-
         CALL flipper_forces_potener()
-        !
-        ! ... initialize the structure factor
-        !
-!~         ntyp_save = ntyp
-!~         ntyp = 1
-!~         CALL struc_fact( nat, tau, ntyp, ityp, ngm, g, bg, &
-!~                    dfftp%nr1, dfftp%nr2, dfftp%nr3, strf, eigts1, eigts2, eigts3 )
-!~         CALL flipper_setlocal()
-!~         ! set ntyp back to what it originally was. Is that necessary?
-!~         ntyp = ntyp_save
-!~         
-!~         flipper_ewld_energy = ewald( alat, nat, ntyp, ityp, zv, at, bg, tau, &
-!~                 omega, g, gg, ngm, gcutm, gstart, gamma_only, strf )
-!~         
-!~         
-!~          CALL flipper_force_ewald( alat, nat, ntyp, ityp, zv, at, bg, tau, omega, g, &
-!~                  gg, ngm, gstart, gamma_only, gcutm, strf )
-!~ 
-!~ 
-!~          CALL flipper_force_lc( nat, tau, ityp, alat, omega, ngm, ngl, igtongl, &
-!~                  g, nl, nspin, gstart, gamma_only, vloc )
-!~ 
-!~         CALL init_us_2( npw, igk, xk(1,1), vkb )
-!~ 
-!~         CALL flipper_force_energy_us (flipper_forcenl, flipper_nlenergy)
-!~         
-!~         DO ipol = 1, 3
-!~              DO na = 1, nr_of_pinballs
-!~                 total_force(ipol,na) = &
-!~                                 flipper_ewald_force(ipol,na)    + &
-!~                                 flipper_forcelc(ipol,na)        + &
-!~                                 flipper_forcenl(ipol, na)
-!~             END DO
-!~         END DO
-!~         
-!~         force(:,:)=0.d0
-!~         do iatom=1,nr_of_pinballs
-!~             force(1:3,iatom)=total_force(1:3,iatom)
-!~         end do
-!~ 9036 FORMAT(5X,'atom ',I4,' type ',I2,'   force = ',3F14.8)
-!~ 
         CALL move_ions()
-        !
-        
-        !
-!~             IF ( istep < nstep .AND. .NOT. conv_ions ) &
-!~                CALL punch( 'config' )
-!~         flipper_cons_qty = flipper_energy_external+flipper_ewld_energy+flipper_energy_kin
-!~         if (ionode) print*, '    FLIPPER: Ekin ', flipper_energy_kin  !Aris
-!~         if (ionode) print*, '    FLIPPER: EWALD ENERGY: ',flipper_ewld_energy  !Aris
-!~         if (ionode) print*, '    FLIPPER: EXTERNAL ENERGY: ',flipper_energy_external  !Aris
-!~         if (ionode) print*, '    FLIPPER: CONSERVED ENERGY', flipper_cons_qty  !Aris
-!~             
-     
+
       ELSE 
          IF ( lforce ) CALL forces()          
          !
